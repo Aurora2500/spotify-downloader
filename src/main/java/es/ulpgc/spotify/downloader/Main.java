f6a1dcda6d134e9ee2f5e709209a18f9a29ceb8e
Aurora <programming.aurora@gmail.com>
2022-10-30 16:46:25 +0000

    init

---
+++ src/main/java/es/ulpgc/spotify/downloader/Main.java
@@ -0,0 +1,14 @@
+package es.ulpgc.spotify.downloader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpotifyAccessor spotify = new SpotifyAccessor();
+
+    }
+
+    //TODO
+    //List<Artist> getArtists(...) filtering by some criteria
+    //List<Album> getAlbums(artist)
+    //List<Track> getTracks(artist),
+}
